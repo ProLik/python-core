f29efd62df09b0e9fab1a2f9c2496ee62327e0e7
lik <lik@pingansec.com>
2017-10-09 17:41:59 +0800

    1

--- com/prolik/python/chapter2.py
+++ com/prolik/python/chapter2.py
@@ -1,7 +1,9 @@
+# coding=utf8
 # target 2.7.14
 
 
 def test():
+    "test function"
 
     print "hello world"
 
@@ -11,7 +13,28 @@ def test():
 
     abs(1)
 
-    print "%s is number %d!" % ("Python", 1)
+    print "%s is number %d! the amount is %f." % ("Python", 1, 0.44)
+
+    import sys
+
+    print >> sys.stderr, "has a error"
+
+    logfile = open("error.txt", "a")
+
+    print >> logfile, "catch a exception!"
+
+    logfile.close()
+
+    # name = raw_input("Enter login name:")
+
+    # print "Your login is:", name
+
+    # num = raw_input("Now enter a number:")
+
+    # print "Doubling your number : %d" % (int(num) * 2)
+
+    # 寻求未知方法的帮助
+    # help(raw_input)
 
 
 if __name__ == '__main__':
