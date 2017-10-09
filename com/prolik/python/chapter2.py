98a4d7c674800b111786e40931a8a2923adea1e1
lik <lik@pingansec.com>
2017-10-09 10:34:34 +0800

    格式输出

--- com/prolik/python/chapter2.py
+++ com/prolik/python/chapter2.py
@@ -1,9 +1,18 @@
 # target 2.7.14
 
-print "hello world"
 
-print(abs(-1))
+def test():
 
-print ("1111111")
+    print "hello world"
 
-abs(1)
+    print(abs(-1))
+
+    print ("1111111")
+
+    abs(1)
+
+    print "%s is number %d!" % ("Python", 1)
+
+
+if __name__ == '__main__':
+    test()
